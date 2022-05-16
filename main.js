98396e8e2e587168476c49c2f298d570f1b983f0
appy-hour <81193735+rebecafelicissimo@users.noreply.github.com>
2022-05-16 13:29:59 -0300

    create about

---
+++ main.js
@@ -0,0 +1,31 @@
+// o scroll de uma página é um evento de scroll.
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stat,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about content`);
